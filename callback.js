d693aabdb8342e5004ddeb40c7ad39283c0a6666
Joshua Christian <jochris1506@gmail.com>
2024-06-26 13:53:38 +0700

    callback:

--- callback.js
+++ callback.js
@@ -1,8 +1,20 @@
-// contoh build in function yang menggunakan callback
-const number1 = [1, 2, 3, 4, 5]
+// // contoh build in function yang menggunakan callback
+// const number1 = [1, 2, 3, 4, 5]
 
-const number2 = number1.map((num) => {
-    return num * 2
+// const number2 = number1.map((num) => {
+//     return num * 2
+// })
+
+// console.log(number2);
+
+// contoh file system yang menggunakan callback
+const fs = require('fs')
+fs.readFile('./data/games.json', 'utf-8', (err, data) => {
+    if (err) console.log(err);
+    else console.log(JSON.parse(data));
 })
 
-console.log(number2);
+fs.readFile('./data/events.json', 'utf-8', (err, data) => {
+    if (err) console.log(err);
+    else console.log(JSON.parse(data));
+})
